afb6a1895ab55fc443207c0b7da6ea8b37ce58d2
student <student@ip-172-31-54-63.us-west-2.compute.internal>
2021-02-11 18:20:31 -0800

    a

--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -1,13 +1,57 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class HousesRunner {
 
 	public static void main(String[] args) {
 		//Here we call the run() method from the Houses class
 		Houses houseBuilder = new Houses();
 		houseBuilder.run();
-
+		drawPointyRoof("medium");
+		grass();
+		drawFlatRoof();
+		grass();
+		
+	}		
+	static void grass() {
+		Robot rob = new Robot();
+		rob.setPenColor(0,250,0);
+		rob.turn(-90);
+		rob.move(25);
+		rob.turn(-90);
+	}
+		static void drawPointyRoof(String Height) {
+int height = 0;
+			if(Height.equals("small")) {
+				height = 60;
+			}
+			else if(Height.equals("medium")) {
+				height = 120;
+			}
+Robot rob = new Robot();
+rob.setSpeed(height);
+rob.setPenColor(255,20,147);
+rob.setX(0);
+rob.setY(450);
+rob.penDown();
+rob.move(100);
+rob.turn(45);
+rob.move(25);
+rob.turn(90);
+rob.move(25);
+rob.turn(45);
+rob.move(100);
 
 	}
+		static void drawFlatRoof() {
+			Robot rob = new Robot();
+			rob.setSpeed(100);
+			rob.move(250);
+			rob.turn(90);
+			rob.move(50);
+			rob.turn(90);
+			rob.move(250);
+		}
 
 }
